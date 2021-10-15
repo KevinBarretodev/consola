2999f7044b6c580a8dd26bd28d6bebda41e8d1a7
Kevin Barreto V <kcbv10@hotmail.com>
2021-10-14 21:53:38 -0500

    mi primer commit oficial

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+
+namespace getRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string API_URL = "https://jsonplaceholder.typicode.com/posts";
+            var client = new WebClient();
+            var json = client.DownloadString(API_URL);
+            dynamic posts = JsonConvert.DeserializeObject(json);
+
+            foreach(var post in posts){
+                Console.WriteLine(post.id+": "+post.title);
+            }
+
+
+        }
+    }
+}
